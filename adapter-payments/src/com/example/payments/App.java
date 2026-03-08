8223a467c6c125b18cd6a60733fa64def2489eb3
Ujjwaljain16 <jainujjwal1609@gmail.com>
2026-03-09 02:08:15 +0530

    completing assignment refactoring

--- adapter-payments/src/com/example/payments/App.java
+++ adapter-payments/src/com/example/payments/App.java
@@ -6,14 +6,15 @@ import java.util.Map;
 public class App {
     public static void main(String[] args) {
         Map<String, PaymentGateway> gateways = new HashMap<>();
-        // TODO: register adapters instead of raw SDKs
-        // gateways.put("fastpay", new FastPayAdapter(new FastPayClient()));
-        // gateways.put("safecash", new SafeCashAdapter(new SafeCashClient()));
-        OrderService svc = new OrderService(gateways);
+        gateways.put("fastpay", new FastPayAdapter(new FastPayClient()));
+        gateways.put("safecash", new SafeCashAdapter(new SafeCashClient()));
 
-        String id1 = svc.charge("fastpay", "cust-1", 1299);
-        String id2 = svc.charge("safecash", "cust-2", 1299);
+        OrderService svc1 = new OrderService(gateways.get("fastpay"));
+        String id1 = svc1.charge("cust-1", 1299);
         System.out.println(id1);
+
+        OrderService svc2 = new OrderService(gateways.get("safecash"));
+        String id2 = svc2.charge("cust-2", 1299);
         System.out.println(id2);
     }
 }
